64c7917be942512cbd6932ae86d58974d1168516
wendellprogram <programacaovila123@gmail.com>
2026-09-01 20:35:39 -0300

    add

---
+++ exercicioperimetro/src/Quadrilatero.java
@@ -0,0 +1,13 @@
+public class Quadrilatero {
+
+        double altura;
+        double largura;
+
+        public double ObterPerimetro() {
+return altura*2+largura*2;
+        }
+
+
+
+    }
+
---
+++ exercicioperimetro/src/principal.java
@@ -0,0 +1,10 @@
+public class principal {
+    public static void main(String[] args) {
+      Quadrilatero q1 = new Quadrilatero();
+            q1.altura = 2;
+            q1.largura = 4;
+        System.out.println("o perimetro é "+q1.ObterPerimetro());
+        }
+    }
+
+
